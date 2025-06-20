12361f564faefa335d5527df3cbabe4765d1afec
sakibtamim <sakib.tamim@gmail.com>
2025-06-20 22:00:02 +0600

    Add Container components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello</h1>
+      <Header />
     </>
   );
 }
---
+++ src/components/Container.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Container = ({ children }) => {
+  return <div className={`max-w-[1120px] mx-auto`}>{children}</div>;
+};
+
+export default Container;
