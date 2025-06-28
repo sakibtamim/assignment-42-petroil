8f57863e7e9e491e8784123bade2ee41e33508c4
sakibtamim <sakib.tamim@gmail.com>
2025-06-28 23:06:33 +0600

    Update Location component to ensure map image takes full width

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -4,7 +4,7 @@ import MapImg from "../assets/map.png";
 const Location = () => {
   return (
     <div className="w-full">
-      <img src={MapImg} alt="" />
+      <img src={MapImg} className="w-full" alt="" />
     </div>
   );
 };
