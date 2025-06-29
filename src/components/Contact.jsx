e8b7c17e9b0d00ef93a1fc6c9c09e0730b68e6c8
sakibtamim <sakib.tamim@gmail.com>
2025-06-29 21:35:25 +0600

    Refactor Contact and Location components for improved layout and responsiveness

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,21 +3,19 @@ import Container from "./Container";
 
 const Contact = () => {
   return (
-    <div className="py-[47px] bg-menuBgColor border-b-4 border-[#FFB800]">
+    <div className="lg:py-[47px] py-[26px] bg-menuBgColor border-b-4 border-[#FFB800]">
       <Container>
         <div className="flex items-center ">
           <div className="w-9/12">
-            <div className="">
-              <h3 className="font-bold font-poppins text-[36px] text-white">
-                Want to join as member branch in your area?
-              </h3>
-            </div>
+            <h3 className="lg:font-bold font-semibold font-poppins lg:text-[36px] text-[14px] text-white">
+              Want to join as member branch in your area?
+            </h3>
           </div>
           <div className="w-3/12">
             <div className=" flex justify-end">
               <a
                 href="#"
-                className="font-poppins text-white py-[14px] px-[32px] border-2 border-white"
+                className="font-poppins text-white lg:py-[14px] lg:px-[32px] py-[6px] px-[10px] lg:border-2 border-1 lg:text-[16px] text-[12px] border-white"
               >
                 CONTACT
               </a>
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -4,7 +4,7 @@ import MapImg from "../assets/map.png";
 const Location = () => {
   return (
     <div className="w-full">
-      <img src={MapImg} className="w-full" alt="" />
+      <img src={MapImg} className="w-full h-[180px] lg:h-auto" alt="" />
     </div>
   );
 };
