37e65c3f448e852b1a909286f36d154fa6f01426
sakibtamim <sakib.tamim@gmail.com>
2025-06-29 15:32:39 +0600

    Refactor LearnAbout component for improved layout and responsiveness

--- src/components/LearnAbout.jsx
+++ src/components/LearnAbout.jsx
@@ -5,19 +5,19 @@ const LearnAbout = () => {
   return (
     <div className="bg-learnAboutBg">
       <Container>
-        <div className="flex py-[136px]">
-          <div className="w-4/12">
-            <div className="pt-[100px] pb-[99px] px-[73px] bg-menuBgColor">
-              <h3 className="text-[36px] font-poppins text-white font-bold">
+        <div className="flex items-center lg:py-[136px] py-[50px]">
+          <div className="lg:w-4/12 w-1/3 ">
+            <div className="lg:pt-[100px] lg:pb-[99px] lg:px-[73px] px-[10px] py-[56px]  bg-menuBgColor">
+              <h3 className="lg:text-[36px] text-[16px] font-poppins text-white font-medium">
                 Learn more about our company
               </h3>
             </div>
           </div>
-          <div className="w-8/12">
-            <div className="bg-[url(/aboutcom.png)] py-[168px] text-center  bg-center bg-cover">
+          <div className="lg:w-8/12 w-2/3">
+            <div className="bg-[url(/aboutcom.png)] lg:py-[168px] py-[80px] text-center  bg-center bg-cover">
               <a
                 href="#"
-                className="py-[14px] bg-white px-[30px] text-[16px] font-poppins text-[#F40404] font-semibold"
+                className="lg:py-[14px] py-[6px] bg-white lg:px-[30px] px-[10px] lg:text-[16px] text-[10px] font-poppins text-[#F40404] font-semibold"
               >
                 Learn More
               </a>
